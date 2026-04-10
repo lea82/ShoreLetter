9ffaef4c7a70daae58eacd265de7734305ab068b
lea82 <liyanhui82@gmail.com>
2026-04-09 17:25:18 -0700

    fix redirect after login

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,6 +45,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|manifest.json|icons|screenshots|api).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|icons|screenshots|api|auth/callback).*)',
   ],
 }
